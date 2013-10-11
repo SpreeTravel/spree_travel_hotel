7092964e4488ff7c632b53665454dbfbde4060c9
Pedro Quintero Rojas <pqr@uh.cu>
2013-10-11 13:34:32 -0400

    ya salen los rates ahora voy para los exeptions

--- app/controllers/spree/admin/rate_hotels_controller.rb
+++ app/controllers/spree/admin/rate_hotels_controller.rb
@@ -2,6 +2,12 @@ module Spree
   module Admin
     class RateHotelsController < ResourceController
 
+      before_filter :load_product
+
+      def load_product
+        @product = Spree::Product.find_by_permalink(params[:permalink])
+      end
+
       def new
         permalink = params[:permalink]
         product = Spree::Product.find_by_permalink(permalink)
@@ -21,6 +27,30 @@ module Spree
             format.html { redirect_to rates_path(permalink) }
           end
         end
+      end
+
+      def update
+        permalink = params[:permalink]
+        product = Spree::Product.find_by_permalink(permalink)
+        begin
+          product.rates.each do |rate|
+            rate.room_id      = params["room_#{rate.id}"]
+            rate.init_date    = params["init_date_#{rate.id}"]
+            rate.end_date     = params["end_date_#{rate.id}"]
+            rate.plan_id      = params["plan_#{rate.id}"]
+            rate.adults_one   = params["adults_one_#{rate.id}"]
+            rate.adults_two   = params["adults_two_#{rate.id}"]
+            rate.adults_three = params["adults_three_#{rate.id}"]
+            rate.children_one = params["children_one_#{rate.id}"]
+            rate.children_two = params["children_two_#{rate.id}"]
+            rate.adults_extra = params["adults_extra_#{rate.id}"]
+            rate.save
+          end
+        #  flash[:success] = "Rate for '#{product.name}' updated successfully"
+        #rescue
+        #  flash[:error] = "Something went wrong while updating rate"
+        end
+        redirect_to rates_path(permalink)
       end
 
       def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Spree::Core::Engine.routes.draw do
   end
 
   match 'admin/products/:permalink/rate_hotels' => 'admin/rate_hotels#index', :via => :get, :as => :rates
+  match 'admin/products/:permalink/update_rate' => 'admin/rate_hotels#update', :via => :post, :as => :update_rate
 
   # Add your extension routes here
 end
