883be4184a74b18fffab2463b0d61941bcf4bb5a
Pedro Quintero Rojas <pqr@uh.cu>
2013-10-13 17:58:17 -0400

    tenia un error en el calculator

--- app/models/spree/calculator_hotel_default.rb
+++ app/models/spree/calculator_hotel_default.rb
@@ -1,16 +1,24 @@
 module Spree
   class CalculatorHotelDefault < TravelCalculator
 
+    # TODO: calcular bien cada dia en que temporada cae
     def self.calculate_price(options = {})
       product = options[:product]
       context = options[:context]
-      combinations = product.combinations
-      combinations = combinations.where(:plan_id => context[:plan_id]) if context[:plan_id]
-      combinations = combinations.where(:init_date => context[:init_date]) if context[:init_date]
-      combinations = combinations.where(:end_date => context[:end_date]) if context[:end_date]
-      combinations = combinations.where(:adults => context[:adults]) if context[:adults]
-      combinations = combinations.where(:children => context[:children]) if context[:children]
+      combinations   = product.combinations
+      combinations   = combinations.where(:room_id => context[:room_id]) if context[:room_id]
+      combinations   = combinations.where(:plan_id => context[:plan_id]) if context[:plan_id]
+      combinations   = combinations.where(:adults => context[:adults]) if context[:adults]
+      combinations   = combinations.where(:children => context[:children]) if context[:children]
+      if context[:init_date] && context[:end_date]
+        combinations = combinations.where('init_date <= ?', context[:init_date]) if context[:init_date]
+        combinations = combinations.where('end_date >= ?' context[:init_date]) if context[:init_date]
+        duration     = context[:end_date] - context[:init_date] + 1
+      else
+        duration     = nil
+      end
       prices = combinations.pluck(:price).sort
+      prices = prices.map {|p| p * duration } if duration
       prices
     end
   end
