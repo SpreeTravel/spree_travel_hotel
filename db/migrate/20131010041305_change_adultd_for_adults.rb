58fc5e27ce05be20a731da5fc3e414445c27212e
Cesar Lage <kaerdsar@gmail.com>
2014-04-14 08:36:37 -0400

    me faltaron algunos ficheros

--- db/migrate/20131010041305_change_adultd_for_adults.rb
+++
@@ -1,5 +0,0 @@
-class ChangeAdultdForAdults < ActiveRecord::Migration
-  def change
-    rename_column :spree_rate_hotels, :adultd_three, :adults_three
-  end
-end
--- db/migrate/20140208094540_change_adults_and_children_column_to_integer.rb
+++
@@ -1,8 +0,0 @@
-class ChangeAdultsAndChildrenColumnToInteger < ActiveRecord::Migration
-  def change
-    remove_column :spree_exception_hotels, :adults
-    remove_column :spree_exception_hotels, :children
-    add_column :spree_exception_hotels, :adults, :integer
-    add_column :spree_exception_hotels, :children, :integer
-  end
-end
