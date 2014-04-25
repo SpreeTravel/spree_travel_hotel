ad62fe0a08fef32e9883211772408ebf011ba6c0
Cesar Lage <kaerdsar@gmail.com>
2014-04-25 11:42:36 -0400

    arreglo en el metodo de calculo de precio de hoteles

--- app/models/spree/calculator_hotel.rb
+++ app/models/spree/calculator_hotel.rb
@@ -1,6 +1,7 @@
 module Spree
   class CalculatorHotel
 
+    # TODO: obtener objeto contexto con los params
     def self.calculate_price(params)
       if params[:variant_id]
         object = Spree::Variant.find(params[:variant_id])
@@ -13,12 +14,31 @@ module Spree
       return [] if pt != params[:product_type]
 
       prices = []
+      pt_start_date = params[pt + '_start_date'].to_date if params[pt + '_start_date']
+      pt_end_date = params[pt + '_end_date'].to_date if params[pt + '_end_date']
+      pt_adult = params[pt + '_adult'].to_i if params[pt + '_adult']
+      pt_child = params[pt + '_child'].to_i if params[pt + '_child']
+      pt_plan = params[pt + '_plan'].to_i if params[pt + '_plan']
+
       object.rates.each do |r|
-        # TODO: los formatos de fecha no son iguales
-        if params[pt + '_start_date'].gsub('-', '/') >= r.start_date && params[pt + '_end_date'].gsub('-', '/') <= r.end_date
-          # TODO: incluir la multiplicacion por la cantidad de dias
-          prices << params[pt + '_adult'].to_i * r.double.to_f
+        next if pt_start_date && (pt_start_date < r.start_date.to_date)
+        next if pt_end_date && (pt_end_date > r.end_date.to_date)
+        next if pt_plan && pt_plan != r.plan.to_i
+        days = pt_end_date - pt_start_date rescue 1
+        price = 0
+        if pt_adult == 1
+            price += pt_adult * r.simple.to_f
+        elsif pt_adult == 2
+            price += pt_adult * r.double.to_f
+        elsif pt_adult == 3
+            price += pt_adult * r.triple.to_f
         end
+        if pt_child == 1
+            price += r.first_child.to_f
+        elsif pt_child == 2
+            price += r.first_child.to_f + r.second_child.to_f
+        end
+        prices << price * days
       end
       prices
     end
