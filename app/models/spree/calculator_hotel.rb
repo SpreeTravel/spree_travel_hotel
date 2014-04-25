d019bb6b43216f189283685afb1ce73e5d65b23e
Cesar Lage <kaerdsar@gmail.com>
2014-04-25 13:23:40 -0400

    arreglando metodo de calculo de hotel para que incluya la variante

--- app/models/spree/calculator_hotel.rb
+++ app/models/spree/calculator_hotel.rb
@@ -3,14 +3,9 @@ module Spree
 
     # TODO: obtener objeto contexto con los params
     def self.calculate_price(params)
-      if params[:variant_id]
-        object = Spree::Variant.find(params[:variant_id])
-      else
-        object = Spree::Product.find(params[:product_id])
-      end
-
-      return [object.price.to_f] if object.rates.empty?
-      pt = object.product_type.name
+      product = Spree::Product.find(params[:product_id])
+      return [product.price.to_f] if product.rates.empty?
+      pt = product.product_type.name
       return [] if pt != params[:product_type]
 
       prices = []
@@ -19,8 +14,10 @@ module Spree
       pt_adult = params[pt + '_adult'].to_i if params[pt + '_adult']
       pt_child = params[pt + '_child'].to_i if params[pt + '_child']
       pt_plan = params[pt + '_plan'].to_i if params[pt + '_plan']
+      pt_variant = Spree::Variant.variant_from_params(params)
 
-      object.rates.each do |r|
+      product.rates.each do |r|
+        next if pt_variant && (pt_variant.id != r.variant_id)
         next if pt_start_date && (pt_start_date < r.start_date.to_date)
         next if pt_end_date && (pt_end_date > r.end_date.to_date)
         next if pt_plan && pt_plan != r.plan.to_i
