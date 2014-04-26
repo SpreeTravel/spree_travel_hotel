38a1674fb321a8a9601eec4fcd27d9493331f882
Cesar Lage <kaerdsar@gmail.com>
2014-04-26 13:26:12 -0400

    mejorando el metodo de calculo de precio de hoteles y eliminando la carga de datos para prototipo

--- app/models/spree/calculator_hotel.rb
+++ app/models/spree/calculator_hotel.rb
@@ -8,7 +8,7 @@ module Spree
       pt = product.product_type.name
       return [] if pt != params[:product_type]
 
-      prices = []
+      adults = {1 => 'simple', 2 => 'double', 3 =>'triple'}
       pt_start_date = params[pt + '_start_date'].to_date if params[pt + '_start_date']
       pt_end_date = params[pt + '_end_date'].to_date if params[pt + '_end_date']
       pt_adult = params[pt + '_adult'].to_i if params[pt + '_adult']
@@ -16,26 +16,17 @@ module Spree
       pt_plan = params[pt + '_plan'].to_i if params[pt + '_plan']
       pt_variant = Spree::Variant.variant_from_params(params)
 
+      prices = []
       product.rates.each do |r|
         next if pt_variant && (pt_variant.id != r.variant_id)
         next if pt_start_date && (pt_start_date < r.start_date.to_date)
         next if pt_end_date && (pt_end_date > r.end_date.to_date)
         next if pt_plan && pt_plan != r.plan.to_i
-        days = pt_end_date - pt_start_date rescue 1
-        price = 0
-        if pt_adult == 1
-            price += pt_adult * r.simple.to_f
-        elsif pt_adult == 2
-            price += pt_adult * r.double.to_f
-        elsif pt_adult == 3
-            price += pt_adult * r.triple.to_f
-        end
-        if pt_child == 1
-            price += r.first_child.to_f
-        elsif pt_child == 2
-            price += r.first_child.to_f + r.second_child.to_f
-        end
-        prices << price * days
+
+        price = pt_adult * r.send(adults[pt_adult]).to_f
+        price += r.first_child.to_f if [1, 2].include?(pt_child)
+        price += r.second_child.to_f if pt_child == 2
+        prices << price * (pt_end_date - pt_start_date rescue 1)
       end
       prices
     end
