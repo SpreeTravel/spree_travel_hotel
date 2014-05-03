b5c602f838e331e220b3ef2c01c8917f63354165
Pedro Quintero Rojas <pqr@uh.cu>
2014-05-03 14:00:43 -0400

    unos cambiecitos

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,9 +1,10 @@
 module Spree
   module BaseHelper
-  
+
       def hotel_stars(product)
-        product.product_properties.where(:property_id => Spree::Property.find_by_name('estrellas')).first.value
-      end 
+        5
+        #product.product_properties.where(:property_id => Spree::Property.find_by_name('estrellas')).first.value
+      end
 
   end
 end
