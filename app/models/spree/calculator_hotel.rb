0fa3d28640647849283603b2a64295a9387a5ad0
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2014-05-26 12:56:45 -0400

    changing the way of calculating the price, not with the context

--- app/models/spree/calculator_hotel.rb
+++ app/models/spree/calculator_hotel.rb
@@ -37,34 +37,37 @@ module Spree
 
     # TODO: obtener objeto contexto con los params
     # TODO: Retornar la variante adem'as del precio
-    def self.calculate_price(params)
-      product = Spree::Product.find(params[:product_id])
+    # TODO: valorar si 'variant' se puede poner como attr opcional para que se calcule dentro
+    def self.calculate_price(context, variant)
+      product = variant.product
       return [product.price.to_f] if product.rates.empty?
-      pt = product.product_type.name
-      return [] if pt != params[:product_type]
+      # pt = product.product_type.name
+      # return [] if pt != context[:product_type]
       # TODO: Think about if adults are more than 3
       # TODO: How to manage if a customer just for fuck enter 1.5 adults
-      pt_start_date = params[pt + '_start_date'].to_date if params[pt + '_start_date']
-      pt_end_date = params[pt + '_end_date'].to_date if params[pt + '_end_date']
-      pt_adult = params[pt + '_adult'] #if params[pt + '_adult']
-      pt_child = params[pt + '_child'] #if params[pt + '_child']
-      pt_plan = params[pt + '_plan'].to_i if params[pt + '_plan']
-      pt_variant = self.calculate_variant(params)
+      # pt_start_date = params[pt + '_start_date'].to_date if params[pt + '_start_date']
+      # pt_end_date = params[pt + '_end_date'].to_date if params[pt + '_end_date']
+      # pt_adult = params[pt + '_adult'] #if params[pt + '_adult']
+      # pt_child = params[pt + '_child'] #if params[pt + '_child']
+      # pt_plan = params[pt + '_plan'].to_i if params[pt + '_plan']
+      # pt_variant = self.calculate_variant(params)
+
+      puts "aqui llego"
 
       prices = []
       # TODO: hacer lo mismo que los adultos y ninos pa los demas campos
       product.rates.each do |r|
-        next if pt_variant && (pt_variant.id != r.variant_id)
-        next if pt_start_date && (pt_start_date < r.start_date.to_date)
-        next if pt_end_date && (pt_end_date > r.end_date.to_date)
-        next if pt_plan && pt_plan != r.plan.to_i
-
-        adults_array = self.get_adult_list(r, pt_adult)
-        children_array = self.get_child_list(r, pt_child)
+        puts "aqui entro"
+        next if variant && (variant.id != r.variant_id)
+        next if context.start_date && (context.start_date.to_date < r.start_date.to_date)
+        next if context.end_date && (context.end_date.to_date > r.end_date.to_date)
+        next if context.plan && context.plan.to_i != r.plan.to_i
+        puts "ando mas abajo"
+        adults_array = self.get_adult_list(r, context.adult)
+        children_array = self.get_child_list(r, context.child)
         combinations = adults_array.product(children_array)
-        puts "=============================================="
-        puts combinations.inspect
         combinations.each do |ad, ch|
+          puts "estoy metiendo precios"
           prices << self.get_rate_price(r, ad, ch)
         end
 
