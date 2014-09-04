59de673cf6d97ed519735a56a49cdbbc79695e94
Alejandro Dustet <aledustet@gmail.com>
2014-09-04 14:40:54 -0400

    Fixed a tipo on the default data loading

--- db/default/spree/product_types.rb
+++ db/default/spree/product_types.rb
@@ -5,12 +5,12 @@ contexts = ['start_date', 'end_date', 'adult', 'child', 'plan']
 context_option_types = contexts.each.map {|c| Spree::OptionType.find_by_name(c)}
 
 variants = ['room']
-varian_option_types = variants.each.map {|v| Spree::OptionType.find_by_name(v)}
+variant_option_types = variants.each.map {|v| Spree::OptionType.find_by_name(v)}
 
 Spree::ProductType.create!(
    :name => 'hotel',
    :presentation => 'Hotel',
    :rate_option_types => rate_option_types,
    :context_option_types => context_option_types,
-   :variat_option_types => variant_option_types
+   :variant_option_types => variant_option_types
 )
