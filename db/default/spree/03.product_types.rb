48e14fb05822c606a68f90e0542da1cb24e4eef1
Pedro Quintero Rojas <pqr@uh.cu>
2014-10-01 18:47:49 -0400

    bug fixing

--- db/default/spree/03.product_types.rb
+++ db/default/spree/03.product_types.rb
@@ -7,10 +7,7 @@ rate_option_types = rates.each.map {|r| Spree::OptionType.find_by_name(r)}
 context_option_types = contexts.each.map {|c| Spree::OptionType.find_by_name(c)}
 variant_option_types = variants.each.map {|v| Spree::OptionType.find_by_name(v)}
 
-### Deleting Product Types
-Spree::ProductType.delete_all
-
-### Creating Product Types
+### Creating Product Type if not created
 Spree::ProductType.where(:name => 'hotel').first_or_create(
    :presentation => 'Hotel',
    :rate_option_types => rate_option_types,
