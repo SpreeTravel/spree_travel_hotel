a8d16b0c6fd48524ac72c39685ed7140d54462b5
Pedro Quintero Rojas <pqr@uh.cu>
2014-10-01 18:13:20 -0400

    deleting option types

--- db/default/spree/01.option_types.rb
+++ db/default/spree/01.option_types.rb
@@ -1,3 +1,4 @@
+### This is the data
 option_types = [
   {:name => "start_date", :presentation => "Start Date", :attr_type => 'date'},
   {:name => "end_date", :presentation => "End Date", :attr_type => 'date'},
@@ -12,6 +13,10 @@ option_types = [
   {:name => "second_child", :presentation => "second_child", :attr_type => 'float'}
 ]
 
+### Deleting Option Types
+Spree::OptionType.delete_all
+
+### Creating Option Types
 option_types.each do |ot|
   Spree::OptionType.where(:name => ot[:name]).first_or_create(:presentation => ot[:presentation], :attr_type => ot[:attr_type])
 end
