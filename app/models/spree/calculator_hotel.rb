08921f4cca065f6c18d3e970fb4ec63b5dbfeb63
Pedro Quintero Rojas <pqr@uh.cu>
2014-11-29 01:28:54 -0500

    adding "combination generation" to hotel

--- app/models/spree/calculator_hotel.rb
+++ app/models/spree/calculator_hotel.rb
@@ -24,27 +24,52 @@ module Spree
       prices
     end
 
-    def self.generate_combinations(product)
-      old_combinations = product.combinations.pluck(:id)
-      new_combinations = []
-      product.rates.each do |r|
-        combination = Spree::Combination.where(:product_id => product.id)
-        combination = combination.where(:start_date => r.start_date.to_date)
-        combination = combination.where(:end_date => r.end_data.to_date)
-        for adults in 1..MAX_ADULTS
-          for children in 1..MAX_CHILDREN
-            for room in product.variants
-              for plan in Spree::OptionType.find_by_name('plan').option_values
-                combination = combination.where(:adults => adults)
-                combination = combination.where(:chilndre => children)
-              end
-            end
+    def self.generate_combination(product, rate)
+      old_combinations  = product.combinations.pluck(:id)
+      keep_combinations = []
+      new_combinations  = []
+      for adults in 1..MAX_ADULTS
+        for children in 1..MAX_CHILDREN
+          room = rate.variant_id
+          plan = rate.plan
+          price = get_rate_price(rate, adults, children)
+          combination = Spree::Combinations.where(:product_id => product.id)
+          combination = combination.where(:start_date => rate.start_date.to_date)
+          combination = combination.where(:end_date => rate.end_date.to_date)
+          combination = combination.where(:adults => adults)
+          combination = combination.where(:children => children)
+          combination = combination.where(:other => combination_string(room, plan))
+          the_combination = combination.first
+          if the_combination
+            keep_combinations << the_combination.id
+          else
+            the_combination = combination.first_or_create!(:price => price)
+            new_combinations << the_combination.id
           end
         end
+      end
+      deleted_combinations = old_combinations - keep_combinations
+      Spree::Combinations.where(:id => deleted_combinations).delete_all
+
+      {
+        :original => old_combinations,
+        :created => new_combinations,
+        :deleted => deleted_combinations,
+        :kept => keep_combinations,
+      }
+    end
+
+    def self.generate_combinations(product)
+      product.rates.each do |r|
+        result = self.generate_combination(product, rate)
       end
     end
 
     private
+
+    def self.combination_string(room, plan)
+      "ROOM:#{room},PLAN:#{plan}"
+    end
 
     def self.get_adult_list(rate, pt_adults)
       if pt_adults.present?
