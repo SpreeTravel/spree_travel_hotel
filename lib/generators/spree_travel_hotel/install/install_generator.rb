dee1a987313287d2a6469f065d527da5331dcb7f
Alejandro Dustet <aledustet@gmail.com>
2014-12-04 18:29:00 -0500

    Modified install generator

--- lib/generators/spree_travel_hotel/install/install_generator.rb
+++ lib/generators/spree_travel_hotel/install/install_generator.rb
@@ -14,17 +14,8 @@ module SpreeTravelHotel
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require admin/spree_travel_hotel\n", :before => /\*\//, :verbose => true
       end
 
-      def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=spree_travel_hotel'
-      end
-
-      def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
-        if run_migrations
-          run 'bundle exec rake db:migrate'
-        else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
-        end
+      def add_seeded_data
+        run 'bundle exec rake spree_travel_hotel:load'
       end
 
     end
