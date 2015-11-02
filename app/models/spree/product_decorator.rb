7f88152c47fe12db95a7b51999a9232f5b8a1873
Pedro Quintero <pecuerre@gmail.com>
2015-11-02 08:33:48 -0500

    removing duplicated method

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,12 +1,6 @@
 # Clase para probar un punto, no debe quedarse, si el cambio hace falta en el core dustet 30/12/2014
 module Spree
   Product.class_eval do
-    after_save :absorb_option_types_new
-
-    def absorb_option_types_new
-      self.option_types = self.product_type.variant_option_types
-    rescue
-    end
 
     def hotel?
       self.product_type == Spree::ProductType.find_by_name('hotel')
