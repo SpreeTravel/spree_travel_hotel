8fb15cbc0b92b8b9215490f35caa8a9eab4bb0e6
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2016-01-30 10:09:13 -0500

    cleaning up a little calculators model

--- app/models/spree/calculator_hotel.rb
+++ app/models/spree/calculator_hotel.rb
@@ -9,10 +9,8 @@ module Spree
       (0..2).to_a
     end
 
-    # TODO preparar esto para que reciba un rate y calcule el precio de un solo producto.....
-    def calculate_price(context, product, variant, options)
-      # TODO hacerlo generico para que se apte cuando hay cambios en los context por el usuario
-      return [product.price.to_f] if product.rates.empty?
+     def calculate_price(context, product, variant, options)
+       return [product.price.to_f] if product.rates.empty?
       # days = context.end_date(options).to_date - context.start_date(options).to_date rescue 1
       # rooms = context.rooms(options).to_i rescue 1
       adults_hash = {1 => 'simple', 2 => 'double', 3 => 'triple'}
@@ -20,37 +18,20 @@ module Spree
       list = product.variants.where(id: variant.id).first.rates
       array = []
       list.each do |r|
-        if r.start_date <= context.start_date(options).to_s && r.end_date >= context.end_date(options).to_s
+        if Date.parse(r.start_date) <= Date.parse(context.start_date(options).to_s) &&
+            Date.parse(r.end_date) >= Date.parse(context.end_date(options).to_s)
+
           avg_price = r.send(adults_hash[context.adult(options).to_i]).to_f
           price = context.adult(options).to_i * avg_price * context.room_count(options).to_i
           price += r.first_child.to_f if context.child(options).to_i >= 1
           price += r.second_child.to_f if context.child(options).to_i == 2
-          # price = price * days * rooms # TODO "x días por cuarto"
+          # price = price * days * rooms # TODO "x days per room"
           array << {price: price, rate: r.id, avg: avg_price}
         end
       end
       array
 
-      # # list = list.where('start_date <= ?', context.start_date(options)) if context.start_date(options).present?
-      # # list = list.where('end_date >= ?', context.end_date(options)) if context.end_date(options).present?
-      # list = list.where(:adults => context.adult(options)) if context.adult(options).present?
-      # list = list.where(:children => context.child(options)) if context.child(options).present?
-      # list = list.where(:room => context.room(options)) if context.room(options).present?
-      # list = list.where(:plan => context.plan(options)) if context.plan(options).present?
-      # list = list.order('price ASC')
-      # Log.debug(list.explain)
-      # list
     end
-
-    # TODO: el problma esta en que la variante es distinta para cada producto
-    # y en el combo en el buscador salen los ID de los option types
-    # y nunca matchea eso
-    # def combination_string_for_generation(rate)
-    #   plan = rate.plan
-    #   raise Exception.new("SOMETHING WRONG") if rate.variant.option_values.count != 1
-    #   room = rate.variant.option_values.first.id
-    #   "ROOM:#{room},PLAN:#{plan}"
-    # end
     
     def get_rate_price(rate, adults, children)
       adults = adults.to_i
